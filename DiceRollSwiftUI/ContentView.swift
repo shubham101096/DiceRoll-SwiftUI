5625c7017a8c8db07bd1a0752daed92d39b2ae49
Shubham Mishra <shubhammishra10@gmail.com>
2021-05-05 16:48:29 +0530

    Setup UI

--- DiceRollSwiftUI/ContentView.swift
+++ DiceRollSwiftUI/ContentView.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: 1)
+                    DiceView(n: 1)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {}) {
+                    Text("Roll")
+                        .foregroundColor(.white)
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
