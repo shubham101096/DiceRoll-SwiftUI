aa00221ca13948aa09b67f4dcc62055ba7174e4f
Shubham Mishra <shubhammishra10@gmail.com>
2021-05-05 14:58:24 +0530

    Initial Commit

---
+++ DiceRollSwiftUI/DiceRollSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollSwiftUIApp.swift
+//  DiceRollSwiftUI
+//
+//  Created by Shubham Mishra on 05/05/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
